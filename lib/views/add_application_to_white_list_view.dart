84e5ee34e3a302c0b20e3ec8f2c89eabffdecad6
Miha976 <aboumihalick@gmail.com>
2026-04-07 11:30:50 +0200

    Adding addview list apps

--- lib/views/add_application_to_white_list_view.dart
+++ lib/views/add_application_to_white_list_view.dart
@@ -1,18 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_device_apps/flutter_device_apps.dart';
 
 class AddApplicationToWhiteListView extends StatefulWidget {
   const AddApplicationToWhiteListView({super.key});
 
   @override
-  State<AddApplicationToWhiteListView> createState () => _AddApplicationToWhiteListViewState(); 
+  State<AddApplicationToWhiteListView> createState() =>
+      _AddApplicationToWhiteListViewState();
 }
 
-class _AddApplicationToWhiteListViewState extends State<AddApplicationToWhiteListView> {
+class _AddApplicationToWhiteListViewState
+    extends State<AddApplicationToWhiteListView> {
+  Future<List<AppInfo>> _getApps() async {
+    return await FlutterDeviceApps.listApps(
+      includeSystem: false,
+      onlyLaunchable: true,
+      includeIcons: true,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text(
+          "Ajouter des applications",
+          style: TextStyle(fontSize: 14),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(child: 
+          FutureBuilder(future: _getApps(), builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: const CircularProgressIndicator());
+            } else {
+              if (!snapshot.hasData) {
+                return Center(child: const Text("Aucune application installée !"));
+              } else {
+                final data = snapshot.data!;
+                return _ListApps(apps: data);
+              }
+            }
+          })
+          )
+        ],
+      ),
+
     );
+  }
+}
+
+class _ListApps extends StatelessWidget {
+  final List<AppInfo> apps;
+
+  const _ListApps({required this.apps});
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: apps.length,
+      itemBuilder: (context, index) {
+      final app = apps[index];
+      return ListTile(title: Text("${app.appName} - ${app.packageName}"),);
+    });
   }
 
 }
